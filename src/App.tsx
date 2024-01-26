c3e091cc3efc464d74f211fc43dd6d05ea7a278f
manhkieu <manh121123@gmail.com>
2024-01-26 21:25:36 +0700

    feat:Delete products with api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react'
-import './App.css'
 import { ProductType } from './Type/ProductType'
-import axios from 'axios'
+import axios, { Axios } from 'axios'
 
 
 function App() {
@@ -32,6 +31,21 @@ function App() {
       GetProduct()
       
     },[])
+   
+    // xóa một item trong api
+    const HanleRemove = (id:number)=>{
+       // call api
+       // cách 1 không cần await
+       axios.delete('http://localhost:3000/products/'+id)
+      // console.log(id)
+      const newProducts= products.filter(item=>item.id !==id) // dùng cái này cũng xóa dc 
+       setProducts(newProducts)
+      // const RemoveItem =async ()=>{
+      //      await axios.delete('http://localhost:3000/products/'+id)
+      // }
+      // RemoveItem()
+                       // nếu muốn xóa thì cần phải chạy lại server db.json
+    } 
 
     
     
@@ -40,7 +54,27 @@ function App() {
     <> 
      <div>{count}</div>
      <button onClick={()=>setCount(count+1)}>Increase</button>
-    <div>{products.map((item,index) =>item.name)}</div>
+     <table>
+       <thead>
+          <th>#</th>
+          <th>Name</th>
+          
+       </thead>
+       <tbody>
+         {products.map((item,index)=>{
+               return(
+                <tr key={index}>
+                    <td>{index+1}</td>
+                    <td>{item.name}</td>
+                    
+                    <td>
+                       <button onClick={()=> HanleRemove(item.id)}>Remove</button>
+                    </td>
+                </tr>
+               )
+         })}
+       </tbody>
+     </table>
       
     </>
   )
